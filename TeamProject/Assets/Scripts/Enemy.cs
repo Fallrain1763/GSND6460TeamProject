d82b5fbc3cf9a59df9042b580442d799e248b71e
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2026-03-24 16:54:36 -0700

    Make enemies chase player via NavMesh, create simple enemy spawner

--- TeamProject/Assets/Scripts/Enemy.cs
+++ TeamProject/Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -6,11 +7,25 @@ public class Enemy : MonoBehaviour
     public float maxHealth = 50f;
     public float damageToPlayer = 10f;
 
+    [Header("Movement")]
+    public Transform target; // Not ideal since this requires manual assignment -- might want to have globally accessible reference instead -RH
+
     float currentHealth;
+    NavMeshAgent agent;
 
     void Start()
     {
         currentHealth = maxHealth;
+        agent = GetComponent<NavMeshAgent>();
+
+        if (target == null) target = GameObject.Find("Player").transform; // I don't like using Find since it's slow but not sure what the best approach is here -RH
+    }
+
+    void Update()
+    {
+        // Not sure whether this should be called every tick
+        // Later, do switch statement for behavior state machine (Move, Attack, Idle, Die?) -RH
+        agent.SetDestination(target.position);
     }
 
     // Called by Bullet on hit
@@ -38,5 +53,10 @@ public class Enemy : MonoBehaviour
         PlayerHealth ph = col.gameObject.GetComponent<PlayerHealth>();
         if (ph != null)
             ph.TakeDamage(damageToPlayer);
+    }
+
+    public void MoveToTarget()
+    {
+         agent.SetDestination(target.position);
     }
 }
