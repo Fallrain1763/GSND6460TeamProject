c0a89e90e405212bd89c389f8bd9e33a29de688f
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2026-03-30 23:50:53 -0700

    Enemy only starts player once in range

--- TeamProject/Assets/Scripts/Enemy/Enemy.cs
+++ TeamProject/Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum EnemyState
+{
+    Idle,
+    Chase,
+    Attack,
+    Die
+}
+
 public class Enemy : MonoBehaviour
 {
     [Header("Stats")]
@@ -11,14 +19,19 @@ public class Enemy : MonoBehaviour
     public Transform target; // Not ideal since this requires manual assignment -- might want to have globally accessible reference instead -RH
     public Animator animator;
 
+    [Header("Targeting")]
+    public float aggroRadius = 10f;
+
     float currentHealth;
     NavMeshAgent agent;
+    EnemyState currentState = EnemyState.Idle;
 
     void Start()
     {
         currentHealth = maxHealth;
         agent = GetComponent<NavMeshAgent>();
 
+        // Currently hardcoded to find player..
         if (target == null) target = GameObject.Find("Player").transform; // I don't like using Find since it's slow but not sure what the best approach is here -RH
     }
 
@@ -26,7 +39,20 @@ public class Enemy : MonoBehaviour
     {
         // Not sure whether this should be called every tick
         // Later, do switch statement for behavior state machine (Move, Attack, Idle, Die?) -RH
-        agent.SetDestination(target.position);
+
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                ScanForTarget();
+                break;
+            case EnemyState.Chase:
+                ChaseTarget();
+                break;
+            case EnemyState.Attack:
+                break;
+            case EnemyState.Die:
+                break;
+        }
     }
 
     // Called by Bullet on hit
@@ -57,8 +83,36 @@ public class Enemy : MonoBehaviour
             ph.TakeDamage(damageToPlayer);
     }
 
+    void ScanForTarget()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, aggroRadius);
+        foreach (Collider hitCollider in hitColliders)
+        {
+            if (hitCollider.gameObject.tag == "Player")
+            {
+                currentState = EnemyState.Chase;
+            }
+        }
+    }
+
+    void ChaseTarget()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    public void ChangeTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
     public void MoveToTarget()
     {
          agent.SetDestination(target.position);
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere (transform.position, aggroRadius);
     }
 }
